bdbf0416427d237a3e1fda5f0f1b64f7cef4b1c4
Ryan M. Waite <ryanwaite28@gmail.com>
2018-05-30 10:27:02 -0400

    initial

---
+++ README.md
@@ -0,0 +1,17 @@
+# Udacity
+### Front End Web Developer
+##### Memory Game
+
+Flip the card to find a match! You get three tries to get them all right!
+
+* Vanilla JS
+* CSS3
+* HTML5
+
+Focuses on DOM manipulation and object-oriented programming.
+
+
+---
+
+# Ryan M. Waite
+#### Web/Mobile Developer
---
+++ js/app.js
@@ -0,0 +1,127 @@
+(function(){
+
+
+  const card = document.getElementsByClassName('card');
+  const allCards = Array.from(card);
+
+  const flippedCards = [];
+  const matchedCards = [];
+
+  // Shuffle function from http://stackoverflow.com/a/2450976
+  function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
+  function createCard() {
+    return (`<li class="card"><i class="fa ${card}"></i></li>`);
+  }
+
+  function start() {
+    const deck = document.querySelector('.deck');
+    let fullDeck = shuffle(allCards).map(function(card) {
+      return createCard(card);
+      console.log('hahaha');
+    });
+  }
+
+  start();
+
+  let movesCount = document.getElementById('moves-count');
+  let starsList = document.getElementById('stars-list');
+  let resetBtn = document.getElementById('reset-btn');
+  let msgText = document.getElementById('msg-text');
+
+  let moves = 3;
+  let isGameOver = false;
+
+  let matches = [];
+  let lastFlipped = null;
+  let pause = false;
+
+  resetBtn.addEventListener('click', function() {
+    resetGame();
+  });
+
+  allCards.forEach(function(card) {
+    card.addEventListener('click', function() {
+      if (card === lastFlipped || matches.includes(card) || pause || isGameOver) {
+        return;
+      }
+
+      card.classList.add('open', 'show');
+
+      if (lastFlipped) {
+        if (card.childNodes[1].classList.item(1) === lastFlipped.childNodes[1].classList.item(1)) {
+          console.log('match found!');
+          matches.push(card);
+          matches.push(lastFlipped);
+          lastFlipped = null;
+          if(matches.length === 16) {
+            gameOver();
+            return;
+          }
+        }
+        else {
+          console.log('no match...');
+          pause = true;
+          moves--;
+          starsList.removeChild(starsList.children[0]);
+          movesCount.innerText = moves;
+          setTimeout(function() {
+            card.classList.remove('open', 'show');
+            lastFlipped.classList.remove('open', 'show');
+            lastFlipped = null;
+            pause = false;
+          }, 1725);
+          if(moves === 0) {
+            gameOver();
+            return;
+          }
+        }
+      } else {
+        lastFlipped = card;
+      }
+    });
+  });
+
+  function gameOver() {
+    let msg = 'Game Over...';
+    console.log(msg);
+    alert(msg);
+    isGameOver = true;
+    msgText.innerText = msg;
+  }
+
+  function resetGame() {
+    moves = 3;
+    isGameOver = false;
+
+    matches = [];
+    lastFlipped = null;
+    pause = false;
+
+    starsList.innerHTML = '';
+    movesCount.innerText = moves;
+    for(var i = 0; i < moves; i++) {
+      starsList.innerHTML += '<li><i class="fa fa-star"></i></li>';
+    }
+    allCards.forEach(function(card) {
+      card.classList.remove('open', 'show');
+    });
+    msgText.innerText = '';
+  }
+
+  // Referenced live webinar walkthrough with Mike Wales, https://developer.mozilla.org and https://www.w3schools.com for functions and process.
+
+
+
+})()
